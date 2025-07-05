4762526a8daba19315ffdeb7bd6b1731da995f2b
zhjckfd1 <44026016+zhjckfd1@users.noreply.github.com>
2025-07-05 18:58:01 +0300

    test refresh

--- ping/src/main/java/com/example/ping/PingApplication.java
+++ ping/src/main/java/com/example/ping/PingApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//Перезапускаем или отправляем POST запрос /actuator/refresh для обновления конфигурации
+
 @SpringBootApplication
 @EnableFeignClients
 public class PingApplication {
